240d0b931eaf8631e718798b89911ca86a6039d9
DevCySide <devcyside@gmail.com>
2025-06-17 19:57:26 +0200

    Add hpot + comsetic changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,8 @@
 function onClickCallback() {
-  var x = document.getElementById("myLinks");
+/*  var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
       x.style.display = "none";
   } else {
       x.style.display = "block";
-  }
+  }*/
 }
